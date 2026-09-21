b67dc899ea88f15bbebfb6fbb2e4b0823cbe8847
remamahmoud66 <remamahmoud66@gmail.com>
2026-09-21 15:18:56 +0300

    Add Mon_21-9-2026 files

---
+++ Mon_21-9-2026/script.js
@@ -0,0 +1,90 @@
+//'use strict'
+
+//var x=10
+//console.log(x);
+//function test_f (x){
+ //var  z=3;
+    //console.log(z);
+//}
+
+//let y=10;
+//z=5; //عشانه حطيت اول سطر ظظ
+//console.log("y=",y);
+//let name = "orange coding ";
+///let num =1;
+//let boolean= true;
+//let true=1; //error
+//let double=2.1;
+//let null_val=null;
+//let unde_val=undefined;
+//let obj_val={name:"john",age:25};
+//let arr_val =[1,2,3,4,5];
+
+//console.log("data type of name",typeof(name));
+//console.log("data type of num",typeof(num));
+//nsole.log("data type of arr_val",typeof(arr_val));
+///////////////operation
+//let a=15;
+//let b=20;
+//let sum =a+b;
+////console.log("sum=a+b=",sum);
+////logic
+//let c=2;
+//let d=10;
+
+//let great_ope= c > b;
+//console.log("and_ope",great_ope);
+//if(c>d){
+   //console.log(c);
+
+
+//}
+//else{
+  //  console.log(d);
+//}
+//let day="sunday"
+//switch(sunday){
+   // case "sunday":
+       // console.log("this our day");
+      //  break;
+   // case "monday":
+   // console.log("this not our day");
+   // default:
+     //   console.log("not found");
+      //  break;
+
+//}
+
+//let string_val="orange academy"
+//console.log("string_val",string_val.toLocaleUpperCase());
+console.log("data type of num",typeof(100));
+console.log("data type of num",typeof(73.9));
+console.log("data type of nan",typeof(NaN));
+console.log("data type of name",typeof("Water"));
+console.log("data type of name",typeof(false));
+console.log("data type of name",typeof(9 != 11));
+console.log("Orang" + "e");
+console.log("Orange" - "s");
+console.log("4" + "8");
+console.log("4" - "8");
+console.log("name" + 3);
+console.log("name" - 3);
+console.log(82 * "word");
+console.log( 1 + "hello");
+console.log("hello" + 1);
+console.log(1 + true);
+console.log( "hello" + true);
+console.log("data type of name",typeof(Infinity));
+console.log( 1 == '1');
+console.log(1 === '1');
+
+
+
+
+
+
+
+
+
+
+
